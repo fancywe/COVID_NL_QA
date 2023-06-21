8532f1100c58d94f506a49adc4890cd90fa19423
fancywe <874576549@qq.com>
2023-06-21 07:36:05 -0500

    not null

--- main.py
+++ main.py
@@ -178,7 +178,9 @@ def read_Q(Q):
                         if code==8 and state=='': 
                                 code=7  
                         if code==10 and state=='' and county=='': 
-                                code=0            
+                                code=0 
+                        if code==1 and state=='' and county=='': 
+                                code=0                       
                         if 0<code<7 and state!='' and county!='': 
                                 code=code+9        
                         if 9<code<15 and state!='' and county=='': 
